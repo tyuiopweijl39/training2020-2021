20873b93d24a251b8c44b0e20ad119821dfc80af
Rick Hallihan <rick.hallihan@gmail.com>
2020-08-15 15:22:48 -0400

    Example Change from Coach Rick

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,15 @@
 /*
 * The HelloWorld Java program
+* 
 */
 class HelloWorld {
   public static void main(String[] args) {
    System.out.println("Hello, FTC 16461!");
+   hiFromCoach();
   }
+
+  public static void hiFromCoach() {
+   System.out.println("Coach Rick was Here");
+  }
+
 }
