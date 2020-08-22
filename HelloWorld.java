0a9c7a8d60fb45780727a5771cf47a3106f142f2
Rick Hallihan <rick@Ricks-Air.lan>
2020-08-22 11:44:25 -0400

    Update HelloWorld with methods for whole team

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,51 @@
-/*
-* The HelloWorld Java program
-* 
-*/
 class HelloWorld {
   public static void main(String[] args) {
-   System.out.println("Hello, FTC 16461!");
-   hiFromCoach();
+    System.out.println("Hello, FTC 16461!");
+    hiFromCoach();
+    hifromamudhan();
+    hifrombrook();
+    hifromdavy();
+    hifromdevan();
+    hifrominiya();
+    hifromjonathan();
+    hifrompavan();
+    hifromryan();
+    hifromsanjita();
+    hifromzoaib();
+
   }
 
   public static void hiFromCoach() {
-   System.out.println("Coach Rick was Here");
+    System.out.println("Coach Rick was Here");
   }
 
+  public static void hifromamudhan() {
+  }
+
+  public static void hifrombrook() {
+  }
+
+  public static void hifromdavy() {
+  }
+
+  public static void hifromdevan() {
+  }
+
+  public static void hifrominiya() {
+  }
+
+  public static void hifromjonathan() {
+  }
+
+  public static void hifrompavan() {
+  }
+
+  public static void hifromryan() {
+  }
+
+  public static void hifromsanjita() {
+  }
+
+  public static void hifromzoaib() {
+  }
 }
